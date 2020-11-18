289486d0d073dd928434077899e038c37a358adc
COM <COM@DESKTOP-TH36TT5>
2020-11-18 19:56:58 +0900

    cartcontroller

---
+++ src/main/java/com/mycompany/webapp/controller/CartController.java
@@ -0,0 +1,23 @@
+package com.mycompany.webapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * 2020. 11. 17 
+ *
+ */
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+	private static final Logger logger = LoggerFactory.getLogger(CartController.class);
+	
+	/*@RequestMapping("/test")*/
+	
+	
+}

2f89959ca8cf3cf0af535b1bd65061ade00dd92e
hyebin <COM@DESKTOP-TH36TT5>
2020-11-18 21:10:50 +0900

    applycontroller 등록

---
+++ src/main/java/com/mycompany/webapp/controller/ApplyController.java
@@ -0,0 +1,10 @@
+package com.mycompany.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/apply")
+public class ApplyController {
+
+}

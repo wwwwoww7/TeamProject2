c58012abec6f8a5a37ececcfefc02cf6a6312005
COM <COM@DESKTOP-TH36TT5>
2020-11-19 18:08:03 +0900

    class controller 수정

--- src/main/java/com/mycompany/webapp/controller/ClassController.java
+++ src/main/java/com/mycompany/webapp/controller/ClassController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,4 +19,27 @@ public class ClassController {
 	public String classDetail(@RequestParam(defaultValue = "1") int classNo) {
 		return "/class/classdetail";
 	}
+	
+	@GetMapping("/classvideo")
+	public String classvideo( ) {
+		return "/class/classvideo";
+	}
+	
+
+	@GetMapping("/classQA")
+	public String classQA( ) {
+		return "/class/classqna";
+	}
+	
+	@PostMapping("/writeQA")
+	public String writeQA() {
+		return "/class/classdetail";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
